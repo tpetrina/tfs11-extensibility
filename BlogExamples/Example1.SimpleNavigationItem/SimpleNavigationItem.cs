8a82bcbce8d30ed2efca693957677d204985bcd5
Toni Petrina <toni.petrina@gmail.com>
2012-11-27 18:50:35 +0100

    Added example code for the blog post no.1

---
+++ BlogExamples/Example1.SimpleNavigationItem/SimpleNavigationItem.cs
@@ -0,0 +1,31 @@
+﻿namespace Example1.SimpleNavigationItem
+{
+    using Microsoft.TeamFoundation.Controls;
+
+    [TeamExplorerNavigationItem("{70871EF0-6965-431B-94C1-8EA914F9E519}", 10000)]
+    public class SimpleNavigationItem : ITeamExplorerNavigationItem
+    {
+        public void Execute() { }
+
+        public System.Drawing.Image Image
+        {
+            get { return null; }
+        }
+
+        public void Invalidate() { }
+
+        public bool IsVisible
+        {
+            get { return true; }
+        }
+
+        public string Text
+        {
+            get { return "Simple navigation link"; }
+        }
+
+        public void Dispose() { }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+    }
+}
