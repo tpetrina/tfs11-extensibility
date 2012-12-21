6e4358a2495d0a4969a8e730416710f444a4d58b
Toni Petrina <toni.petrina@gmail.com>
2012-12-21 15:47:19 +0100

    Started witadmin visual project

---
+++ Extensions/AdminTools.Commands/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AdminTools.Commands
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        protected bool SetAndRaise<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Extensions/AdminTools.Commands/WitActionsNavigationItem.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Controls;
+using Microsoft.VisualStudio.Shell;
+
+namespace AdminTools.Commands
+{
+    [TeamExplorerNavigationItem("{CD269465-EBD6-4DF4-87FA-448D4B3C4782}", 11000)]
+    public class WitActionsNavigationItem : ViewModelBase, ITeamExplorerNavigationItem
+    {
+        private ITeamFoundationContextManager _contextManager;
+
+        [Import]
+        public SVsServiceProvider ServiceProvider { get; set; }
+
+        private bool _isVisible = true;
+        private string _text = "Admin Tools";
+
+        public System.Drawing.Image Image
+        {
+            get { return null; }
+        }
+
+        public bool IsVisible
+        {
+            get { return _isVisible; }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set { SetAndRaise(ref _text, value); }
+        }
+
+        public void Invalidate() { }
+
+        public void Execute() { }
+
+        public void Dispose() { }
+
+    }
+}
