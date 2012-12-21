da6cbb1ba1d5055a33f32f511c5e191643d0ffee
Toni Petrina <toni.petrina@gmail.com>
2012-12-21 15:46:35 +0100

    Example code for the third blog post

---
+++ BlogExamples/Example3.NewPage/NavigationItem.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.Composition;
+using System.Drawing;
+using Microsoft.TeamFoundation.Controls;
+using Microsoft.TeamFoundation.Controls.WPF.TeamExplorer;
+using Microsoft.VisualStudio.Shell;
+
+namespace Example3.NewPage
+{
+    [TeamExplorerNavigationItem("{E9C94375-3287-4CBC-9BFE-5822CCA62D2C}", 500, TargetPageId = "{4A8ECFE5-6B30-43E0-AE85-6C8A2C960C4D}")]
+    public class NavigationItem : ITeamExplorerNavigationItem
+    {
+        [Import]
+        public SVsServiceProvider ServiceProvider { get; set; }
+
+        public void Execute()
+        {
+            TeamExplorerUtils.Instance.NavigateToPage("4A8ECFE5-6B30-43E0-AE85-6C8A2C960C4D", ServiceProvider, null);
+        }
+
+        public Image Image
+        {
+            get { return null; }
+        }
+
+        public void Invalidate()
+        {
+        }
+
+        public bool IsVisible
+        {
+            get { return true; }
+        }
+
+        public string Text
+        {
+            get { return "Go to My Page"; }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ BlogExamples/Example3.NewPage/Page.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Controls;
+
+namespace Example3.NewPage
+{
+    [TeamExplorerPage("{4A8ECFE5-6B30-43E0-AE85-6C8A2C960C4D}")]
+    public class Page : ITeamExplorerPage
+    {
+        private string _title = "Info page";
+        PageView _pageContent = null;
+
+        public bool IsBusy
+        {
+            get { return false; }
+        }
+
+        public object PageContent
+        {
+            get
+            {
+                if (_pageContent == null)
+                    _pageContent = new PageView();
+
+                return _pageContent;
+            }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public void Cancel()
+        {
+        }
+
+        public object GetExtensibilityService(Type serviceType)
+        {
+            return null;
+        }
+
+        public void Initialize(object sender, PageInitializeEventArgs e)
+        {
+        }
+
+        public void Loaded(object sender, PageLoadedEventArgs e)
+        {
+        }
+
+        public void Refresh()
+        {
+        }
+
+        public void SaveContext(object sender, PageSaveContextEventArgs e)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+    }
+}
